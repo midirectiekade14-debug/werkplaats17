633bc803586bdfecea4b905f4def7e777d8195f6
midirectiekade14-debug <midirectiekade14@gmail.com>
2026-08-16 19:48:39 +0200

    Content bijgewerkt via admin (16-8-2026, 19:48:38)

--- docs/content.js
+++ docs/content.js
@@ -179,8 +179,8 @@ const CONTENT = {
       "color": "#E09A2F"
     },
     "offerText": {
-      "color": "#F5B040",
-      "size": 130
+      "color": "#F0EAE0",
+      "size": 115
     },
     "pricingLabel": {
       "size": 130,
@@ -197,7 +197,8 @@ const CONTENT = {
       "color": "#F0EAE0"
     },
     "offerTitle": {
-      "color": "#F0EAE0"
+      "color": "#F5B040",
+      "size": 105
     }
   }
 };
